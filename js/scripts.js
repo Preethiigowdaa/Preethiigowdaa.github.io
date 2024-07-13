58189735c7285d467e2f241c73f25e0a4242ee46
Preethigowda20 <105908920+Preethiigowdaa@users.noreply.github.com>
2024-07-13 21:15:40 +0530

    Added js file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Modal functionality
+const modal = document.getElementById("contact-form-modal");
+const btn = document.getElementById("contact-form-link");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
